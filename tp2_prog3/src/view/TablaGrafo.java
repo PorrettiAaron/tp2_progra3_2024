42c119309122ec6b7462e9bc56c32f14c87ac32a
Federico Buen <88689636+FedeBuen@users.noreply.github.com>
2024-05-12 22:06:30 -0300

    Modificado Tabla y tablaGrafo
    agregado opciones para ver detalles en Tabla y TablaGrafo

--- tp2_prog3/src/view/TablaGrafo.java
+++ tp2_prog3/src/view/TablaGrafo.java
@@ -1,7 +1,10 @@
 package view;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Map.Entry;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SwingUtilities;
@@ -16,20 +19,34 @@ public class TablaGrafo extends JFrame {
 		SwingUtilities.invokeLater(() -> {
 			JFrame frame = new JFrame("Tabla del Grafo");
 			frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			frame.setSize(400, 300);
+			frame.setSize(600, 300);
 			frame.setLocationRelativeTo(null);
 			Grafo g = grafo;
 			String[] columnas = { "Vertice", "Adyacencias" };
+		
 			DefaultTableModel tableModel = new DefaultTableModel(columnas, 0);
 			for (Entry<String, String> entry : g.imprimirGrafo().entrySet()) {
 				tableModel.addRow(new Object[] { entry.getKey(), entry.getValue() });
 			}
 			JTable table = new JTable(tableModel);
+	        // Añadir un MouseListener para detectar clics en las filas
+	        table.addMouseListener(new MouseAdapter() {
+	            @Override
+	            public void mouseClicked(MouseEvent e) {
+	                if (e.getClickCount() == 1) {
+	                    JTable target = (JTable) e.getSource();
+	                    int row = target.getSelectedRow();
+	                    String vertice = (String) target.getValueAt(row, 0);
+	                    String col = (String) target.getValueAt(row, 1);
+	                    JOptionPane.showMessageDialog(frame, col, "Adyacencias de " + vertice ,JOptionPane.INFORMATION_MESSAGE);
+	                }
+	            }
+	        });
+	    	table.getColumnModel().getColumn(0).setPreferredWidth(100);
+	    	table.getColumnModel().getColumn(1).setPreferredWidth(500);
 			JScrollPane scrollPane = new JScrollPane(table);
 			frame.add(scrollPane);
-
 			frame.setVisible(true);
-
 		});
 	}
 
